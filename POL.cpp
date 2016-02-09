091762d58a8100b359722ccbd7984fc014679ffa
Jacek Siekierski <siekier@gmail.com>
2016-02-09 18:00:16 +0100

    Zadanie rozpoczete, nie wiem jak zrobic...

---
+++ POL.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int t;
+string ciag;
+
+int main()
+{
+	cin>>t;
+	for(int i=2; i<=t+1; i++)
+	{
+		cin>>ciag;
+		
+
+
+	
+	}
+	return 0;
+}
+
